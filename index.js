7a4b7649724e03713f452c585b7b47efd2090c97
MelaniaPicone <melania_picone@yahoo.it>
2025-07-31 09:44:09 +0200

    [add] bonus corr

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log(process.argv[2]);
-console.log("Hello World");
+//console.log(process.argv[2]);
+//console.log("Hello World");
+
+let param = process.argv[2] != undefined ? process.argv[2] : "Boolean"
+
+console.log(`Hello ${param}`);
